64b580514b782fec7783c8ddf642b31b2a04394f
ThomasCoyle12 <163637691+ThomasCoyle12@users.noreply.github.com>
2024-03-17 22:05:23 +1100

    Update filters

--- lib/my-address-api-stack.ts
+++ lib/my-address-api-stack.ts
@@ -1,38 +1,51 @@
-import { Stack, aws_dynamodb as dynamodb, aws_lambda as lambda, StackProps, RemovalPolicy } from 'aws-cdk-lib';
+import { Stack, StackProps, RemovalPolicy } from 'aws-cdk-lib';
 import { Construct } from 'constructs';
 import * as path from 'path';
+import { Table, AttributeType, BillingMode } from 'aws-cdk-lib/aws-dynamodb';
+import { Function, Runtime, Code } from 'aws-cdk-lib/aws-lambda';
+import { RestApi, LambdaIntegration } from 'aws-cdk-lib/aws-apigateway';
 
 export class MyApiStack extends Stack {
   constructor(scope: Construct, id: string, props?: StackProps) {
     super(scope, id, props);
 
-    const userAddressesTable = new dynamodb.Table(this, 'UserAddresses', {
-      partitionKey: { name: 'userId', type: dynamodb.AttributeType.STRING },
-      sortKey: { name: 'addressId', type: dynamodb.AttributeType.STRING },
+    const userAddressesTable = new Table(this, 'UserAddresses', {
+      partitionKey: { name: 'userId', type: AttributeType.STRING },
+      sortKey: { name: 'addressId', type: AttributeType.STRING },
       tableName: 'UserAddresses',
-      billingMode: dynamodb.BillingMode.PAY_PER_REQUEST,
+      billingMode: BillingMode.PAY_PER_REQUEST,
       removalPolicy: RemovalPolicy.DESTROY,
     });
 
-    const AddAddressFunction = new lambda.Function(this, 'AddAddressFunction', {
-      runtime: lambda.Runtime.NODEJS_16_X,
+    const addAddressFunction = new Function(this, 'AddAddressFunction', {
+      runtime: Runtime.NODEJS_16_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../src/addAddress')),
+      code: Code.fromAsset(path.join(__dirname, '../src/addAddress')),
       environment: {
         TABLE_NAME: userAddressesTable.tableName,
       },
     });
 
-    const GetAddressFunction = new lambda.Function(this, 'GetAddressFunction', {
-      runtime: lambda.Runtime.NODEJS_16_X,
+    const getAddressesFunction = new Function(this, 'GetAddressFunction', {
+      runtime: Runtime.NODEJS_16_X,
       handler: 'index.handler',
-      code: lambda.Code.fromAsset(path.join(__dirname, '../src/getAddress')),
+      code: Code.fromAsset(path.join(__dirname, '../src/getAddress')),
       environment: {
         TABLE_NAME: userAddressesTable.tableName,
       },
     });
 
-    userAddressesTable.grantReadWriteData(AddAddressFunction);
-    userAddressesTable.grantReadData(GetAddressFunction);
+    userAddressesTable.grantReadWriteData(addAddressFunction);
+    userAddressesTable.grantReadData(getAddressesFunction);
+
+    const api = new RestApi(this, 'UserAddressApi', {
+      restApiName: 'User Address Service',
+    });
+
+    const addresses = api.root.addResource('addresses');
+    addresses.addMethod('POST', new LambdaIntegration(addAddressFunction));
+
+    const userAddresses = addresses.addResource('{userId}');
+    userAddresses.addMethod('GET', new LambdaIntegration(getAddressesFunction));
   }
 }
--- src/getAddress/index.ts
+++ src/getAddress/index.ts
@@ -1,18 +1,35 @@
-import { DynamoDB } from '@aws-sdk/client-dynamodb';
-
+import { DynamoDB, QueryCommand, QueryCommandInput  } from '@aws-sdk/client-dynamodb';
 const dynamoDBClient = new DynamoDB({});
-
 async function getAddress(event: any) {
   const { userId, suburb, postcode } = event.queryStringParameters || {};
-  const params = {
-    TableName: process.env.TABLE_NAME,
-    KeyConditionExpression: 'userId = :userId',
-    ExpressionAttributeValues: {
-      ':userId': { S: userId },
-    },
+  let filterExpressions = [];
+  const expressionAttributeValues: any = {
+    ':userId': { S: userId },
   };
 
-  const { Items } = await dynamoDBClient.query(params);
+  if (suburb) {
+    filterExpressions.push('suburb = :suburb');
+    expressionAttributeValues[':suburb'] = { S: suburb };
+  }
+
+  if (postcode) {
+    filterExpressions.push('postcode = :postcode');
+    expressionAttributeValues[':postcode'] = { S: postcode };
+  }
+
+  const params: QueryCommandInput = {
+    TableName: process.env.TABLE_NAME,
+    KeyConditionExpression: 'userId = :userId',
+    ExpressionAttributeValues: expressionAttributeValues,
+  };
+
+  if (filterExpressions.length > 0) {
+    params.FilterExpression = filterExpressions.join(' AND ');
+  }
+
+  const command = new QueryCommand(params);
+
+  const { Items } = await dynamoDBClient.send(command);
 
   return {
     statusCode: 200,
